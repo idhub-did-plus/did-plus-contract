60381e2e3864167560cf9ba6f104705b96562a40
piouslove <305053045@qq.com>
2019-08-19 12:45:11 +0800

    ropsten network config

--- migrations/2_did_contract_deploy.js
+++
@@ -1,7 +0,0 @@
-const IdentityRegistry = artifacts.require("IdentityRegistry");
-const EthereumDIDRegistry = artifacts.require("EthereumDIDRegistry");
-
-module.exports = function(deployer) {
-    deployer.deploy(IdentityRegistry);
-    deployer.deploy(EthereumDIDRegistry);
-};
